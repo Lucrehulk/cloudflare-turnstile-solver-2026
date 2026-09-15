67554985c14bb03378bad6cd28ac300a994188d9
Lucrehulk <Rotobaga365@gmail.com>
2026-09-15 06:44:41 -0400

    Change z-order enforcement loop rate to 200ms
    Updated the enforce z-order loop rate from 100ms to 200ms to optimize performance.

--- cf-turnstile-bypass/z-index-orderer/src/main.rs
+++ cf-turnstile-bypass/z-index-orderer/src/main.rs
@@ -5,7 +5,7 @@
 #![cfg(target_os = "windows")]
 
 // Config for enforce z order loop rate.
-const ENFORCE_Z_ORDER_RATE: u64 = 100;
+const ENFORCE_Z_ORDER_RATE: u64 = 200;
 
 use std::{
     collections::HashSet,
